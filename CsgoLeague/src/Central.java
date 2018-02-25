ffec1f672c154a2ce9e8d39fcb6a03fa94324913
BuildTools <unconfigured@null.spigotmc.org>
2018-02-25 17:57:52 -0500

    added java

---
+++ CsgoLeague/src/Central.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jack on 2/18/2018.
+ */
+public class Central {
+
+    public static void main(String args){
+
+    }
+
+}
---
+++ CsgoLeague/src/GUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Jack on 2/18/2018.
+ */
+public class GUI extends JFrame implements ActionListener {
+
+    
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+}
