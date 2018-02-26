0b6f62a42ae93868b8e9fc63ff146f193fca6666
Jack Fountain <jackdkdkf@gmail.com>
2018-02-26 15:25:25 -0500

    Created new gui

---
+++ CsgoLeague/src/GUI/Leaderboard.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Jack on 2/26/2018.
+ */
+public class Leaderboard extends JFrame implements ActionListener {
+}
---
+++ CsgoLeague/src/GUI/Login.java
@@ -0,0 +1,22 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Jack on 2/26/2018.
+ */
+public class Login extends JFrame implements ActionListener {
+
+    public Login(){
+        super("Client");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+}
+
