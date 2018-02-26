a9167b3f762751b6df4fabc5bed2dc21bc7fddcf
BuildTools <unconfigured@null.spigotmc.org>
2018-02-25 22:25:00 -0500

    Started gui

--- CsgoLeague/src/Central.java
+++ CsgoLeague/src/Central.java
@@ -1,10 +1,17 @@
+import javax.swing.*;
+
 /**
  * Created by Jack on 2/18/2018.
  */
 public class Central {
 
-    public static void main(String args){
-
+    public static void main(String[] args){
+        GUI gui = new GUI();
+        gui.setSize(900, 500);
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setResizable(false);
+        gui.setLayout(null);
+        gui.setVisible(true);
     }
 
 }
--- CsgoLeague/src/GUI.java
+++ CsgoLeague/src/GUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -7,7 +8,47 @@ import java.awt.event.ActionListener;
  */
 public class GUI extends JFrame implements ActionListener {
 
-    
+    private JButton login, standings, stats, startScrim;
+    private final int buttonHeight = 10;
+
+    public GUI(){
+        super("Client");
+
+        //Login button or logout button
+        login = new JButton("Login");
+        login.addActionListener(this);
+        login.setActionCommand("Login");
+        login.setLocation(15, buttonHeight);
+        login.setFont(new Font("Ubuntu", Font.PLAIN, 30));
+        login.setSize(150,40);
+        //See teams and players leaderboard
+        standings = new JButton("Standings");
+        standings.addActionListener(this);
+        standings.setActionCommand("Standings");
+        standings.setLocation(180, buttonHeight);
+        standings.setFont(new Font("Ubuntu", Font.PLAIN, 30));
+        standings.setSize(200,40);
+        //See your personal stats
+        stats = new JButton("Statistics");
+        stats.addActionListener(this);
+        stats.setActionCommand("Statistics");
+        stats.setLocation(395,buttonHeight);
+        stats.setFont(new Font("Ubuntu", Font.PLAIN, 30));
+        stats.setSize(200, 40);
+        //Starts a scrim with another team
+        startScrim = new JButton("Start Scrim");
+        startScrim.addActionListener(this);
+        startScrim.setActionCommand("Search");
+        startScrim.setLocation(15, 80);
+        startScrim.setFont(new Font("Ubuntu", Font.PLAIN, 80));
+        startScrim.setSize(580, 90);
+
+        //Adds all the objects
+        add(login);
+        add(standings);
+        add(stats);
+        add(startScrim);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e){
