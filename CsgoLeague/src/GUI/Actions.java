f603e1786b3cb9bf778bd96ff8f970ab7cab4ed5
Jack Fountain <jackdkdf@gmail.com>
2018-03-01 18:55:15 -0500

    kachow

---
+++ CsgoLeague/src/GUI/Actions.java
@@ -0,0 +1,7 @@
+package GUI;
+
+/**
+ * Created by Jack on 3/1/2018.
+ */
+public class Actions {
+}
---
+++ CsgoLeague/src/GUI/StartMenuPanels/CreateMatch.java
@@ -0,0 +1,7 @@
+package GUI.StartMenuPanels;
+
+/**
+ * Created by Jack on 3/1/2018.
+ */
+public class CreateMatch {
+}
