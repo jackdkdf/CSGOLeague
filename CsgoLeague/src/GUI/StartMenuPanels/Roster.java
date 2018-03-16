855aab292bd4b8523dc88c293710a1c228b6ae5c
Jack Fountain <jackdkdf@gmail.com>
2018-03-15 21:38:00 -0400

    stil GUI

--- CsgoLeague/src/GUI/StartMenuPanels/Roster.java
+++ CsgoLeague/src/GUI/StartMenuPanels/Roster.java
@@ -14,7 +14,7 @@ public class Roster {
     JMethods jMethods = new JMethods();
     Colors colors = new Colors();
 
-    JPanel players, ready;
+    JPanel players;
 
     private JLabel player1, player2, player3, player4, player5, button2, button3, button4, button5;
     private JButton button1;
@@ -25,11 +25,6 @@ public class Roster {
         //Player names container
         players = jMethods.createJPanel(650, 150, 25, 120, colors.sideBar);
         players.setLayout(new GridLayout(2,5));
-//        player1 = jMethods.createTestJLabel("Jack", 25, colors.playerText);
-//        player2 = jMethods.createTestJLabel("Powerr", 25, colors.playerText);
-//        player3 = jMethods.createTestJLabel("Cyrus", 25, colors.playerText);
-//        player4 = jMethods.createTestJLabel("Cyber", 25, colors.playerText);
-//        player5 = jMethods.createTestJLabel("SMileyFace", 25, colors.playerText);
 
         player1 = jMethods.createPlayerJLabel("Jack", colors.playerText);
         player2 = jMethods.createPlayerJLabel("Powerr", colors.playerText);
@@ -43,20 +38,7 @@ public class Roster {
         players.add(player4);
         players.add(player5);
 
-        //Ready buttons container
-//        ready = jMethods.createJPanel(650, 100, 25, 170, colors.sideBar);
-//        ready.setLayout(new FlowLayout(1));
-//        button1 = jMethods.createTestJButton("Ready up", 15, colors.playerText);
-//        button2 = jMethods.createTestJButton("Ready up", 15, colors.playerText);
-//        button3 = jMethods.createTestJButton("Ready up", 15, colors.playerText);
-//        button4 = jMethods.createTestJButton("Ready up", 15, colors.playerText);
-//        button5 = jMethods.createTestJButton("Ready up", 15, colors.playerText);
         button1 = jMethods.createJButton("Ready up", 15, 100, 20, Color.white, Color.BLUE);
-//        button2 = jMethods.createJButton("Ready up", 15, 100, 20, Color.white, Color.BLUE);
-//        button3 = jMethods.createJButton("Ready up", 15, 100, 20, Color.white, Color.BLUE);
-//        button4 = jMethods.createJButton("Ready up", 15, 100, 20, Color.white, Color.BLUE);
-//        button5 = jMethods.createJButton("Ready up", 15, 100, 20, Color.white, Color.BLUE);
-
         button2 = jMethods.createTestPlayerJLabel("Ready", 15, Color.BLUE, Color.WHITE);
         button3 = jMethods.createTestPlayerJLabel("Ready", 15, Color.BLUE, Color.WHITE);
         button4 = jMethods.createTestPlayerJLabel("Ready", 15, Color.BLUE, Color.WHITE);
@@ -68,19 +50,9 @@ public class Roster {
         players.add(button4);
         players.add(button5);
 
-//        ready.add(button1);
-//        ready.add(button2);
-//        ready.add(button3);
-//        ready.add(button4);
-//        ready.add(button5);
-
     }
 
     public JPanel getPlayers() {
         return players;
-    }
-
-    public JPanel getReady() {
-        return ready;
     }
 }
--- CsgoLeague/src/GUI/StartMenuPanels/SideBar.java
+++ CsgoLeague/src/GUI/StartMenuPanels/SideBar.java
@@ -14,11 +14,11 @@ public class SideBar {
 
     JPanel User, team, matches;
 
-    private  JLabel name, profilePic, currentTeam, myTeam, upcomingEvents;
+    private  JLabel name, profilePic, currentTeam, myTeam, teamStanding, upcomingEvents;
     private JButton statusOnline, statusOffline;
 
     public void initSideBar(){
-        User = jMethods.createJPanel(300, 200, 700, 0, colors.sideBar);
+        User = jMethods.createJPanel(300, 100, 700, 0, colors.sideBar);
         User.setLayout(null);
         //Profile picture scaling
         profilePic = jMethods.createJLabel("", 0, 10, 30, 60, 60, Color.BLACK);
@@ -41,10 +41,18 @@ public class SideBar {
         User.add(statusOnline);
         User.add(statusOffline);
 
-        team = jMethods.createJPanel(300, 200, 700, 200, colors.sideBar);
+        team = jMethods.createJPanel(300, 100, 700, 100, colors.sideBar);
+        team.setLayout(new BoxLayout(team, BoxLayout.PAGE_AXIS));
 
-        matches = jMethods.createJPanel(300, 200, 700, 400, colors.sideBar);
+        currentTeam = jMethods.createTestJLabel("Current Team", 22, Color.white);
+        myTeam = jMethods.createTestJLabel("MARVELOUS GAMING", 15, Color.white);
+        teamStanding = jMethods.createTestJLabel("3rd place in league", 15, Color.YELLOW);
 
+        team.add(currentTeam);
+        team.add(myTeam);
+        team.add(teamStanding);
+
+        matches = jMethods.createJPanel(300, 400, 700, 200, colors.sideBar);
     }
 
     public JPanel getMatches() {
